8931fcd7418aa94c663104932a82a3fe2de5e778
TOGARU BHARGAV <94062752+Bhargavtogaru@users.noreply.github.com>
2025-03-03 23:03:49 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,31 @@
 //your JS code here. If required.
+function daysOfAYear(n) {
+	let x=365;
+	let y=366;
+	if(n%4 === 0)
+	{
+		if(n%100 === 0)
+		{
+			if(n%400 === 0)
+				{
+				return y;
+				}
+			else
+				{
+				return x;
+				}
+		}
+		else
+		{
+			return y;
+		}
+	}
+	else{
+		return x;
+	}
+	
+	}
+
+daysOfAYear(2002);
+	
+}
